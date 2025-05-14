d4858fe65b67096e240e81b36157d928af529b89
Fatihul Ihsan <151743371+Ihsenpai@users.noreply.github.com>
2025-05-15 06:38:50 +0700

    Add files via upload

---
+++ HasilPerceptron.m
@@ -0,0 +1,54 @@
+Data Input (P):
+ 0 0
+ 0 1
+ 1 0
+ 1 1
+Target (T):
+ 0
+ 1
+ 1
+ 1
+Bobot Input Awal (W): 0.00 0.00
+Bobot Bias Awal (b): 0.00
+
+Epoh ke-1
+Output Jaringan (Y): 1111
+Error Pelatihan (E):-1000
+Bobot Input Baru (W) : 0.00 0.00
+Bobot Bias baru (b)  : -1.00
+Sum Square Error (SSE): 1.00
+
+Epoh ke-2
+Output Jaringan (Y): 0000
+Error Pelatihan (E):0111
+Bobot Input Baru (W) : 2.00 2.00
+Bobot Bias baru (b)  : 2.00
+Sum Square Error (SSE): 3.00
+
+Epoh ke-3
+Output Jaringan (Y): 1111
+Error Pelatihan (E):-1000
+Bobot Input Baru (W) : 2.00 2.00
+Bobot Bias baru (b)  : 1.00
+Sum Square Error (SSE): 1.00
+
+Epoh ke-4
+Output Jaringan (Y): 1111
+Error Pelatihan (E):-1000
+Bobot Input Baru (W) : 2.00 2.00
+Bobot Bias baru (b)  : 0.00
+Sum Square Error (SSE): 1.00
+
+Epoh ke-5
+Output Jaringan (Y): 1111
+Error Pelatihan (E):-1000
+Bobot Input Baru (W) : 2.00 2.00
+Bobot Bias baru (b)  : -1.00
+Sum Square Error (SSE): 1.00
+
+Epoh ke-6
+Output Jaringan (Y): 0111
+Error Pelatihan (E):0000
+Bobot Input Baru (W) : 2.00 2.00
+Bobot Bias baru (b)  : -1.00
+Sum Square Error (SSE): 0.00
